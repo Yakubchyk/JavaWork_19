421ec4b8c15c47bc25b963f8a52b93ddda997c82
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-05-29 12:13:58 +0200

    ClassUnterricht Logisch

--- src/com/yk3_Multithreading/Main.java
+++ src/com/yk3_Multithreading/Main.java
@@ -7,12 +7,24 @@ public class Main {
         MyThread1 thread1 = new MyThread1();
         MyThread2 thread2 = new MyThread2();
         Thread threadMain = new Thread();
-        System.out.println("MAIN -->");
         threadMain.start();
+        System.out.println("MAIN -->");
+        int m = 10;
+        for (int i = 0; i < m; i++) {
+            System.out.println("MAIN " + i);
+            if (i % 2 == 0) {
+                System.out.println("MAIN * 2 = " + i);
+            }
+
+        }
         threadMain.join();
+
         thread.start();
+        thread.join();
         thread1.start();
+        thread1.join();
         thread2.start();
+        thread2.join();
         System.out.println("MAIN <--");
     }
 }
